d236427dbd76509c87d6ffcbaa963c87c148d919
EDUARDO ROSA <eduardo.g.o.rosa@gmail.com>
2023-04-08 19:52:50 -0300

    atualizacao

--- bTree.py
+++ bTree.py
@@ -4,88 +4,103 @@ import read_csv_save_binary as data
 
 data_file = 'data.bin'
 index_file = 'index.bin'
-order = 4
+inserted = 0
+
 
 class BTreeNode:
     def __init__(self, leaf=False):
-        self.keys = []
-        self.children = []
         self.leaf = leaf
-
-    def is_full(self, order):
-        return len(self.keys) >= (2*order)-1
-
+        self.keys = []
+        self.child = []
 
 class BTree:
-    def __init__(self, order=3):
+    def __init__(self, t):
         self.root = BTreeNode(True)
-        self.order = order
-    
-    def search(self, key, node = None):
-        node = self.root if node == None else None
-
-        i = 0
-        while i < len(node.keys) and key > node.keys[i]:
-            i+=1
-        if i < len(node.keys) and key == node.keys[i]:
-            return (node, i)
-        elif node.leaf:
-            return None
-        else:
-            return self.search(key, node.children[i])
-        
-    def split_child(self, x, i):
-        t = self.order
-
-        y = x.children[i]
-
-        z = BTreeNode(y.leaf)
-        x.children.insert(i + 1, z)
-
-        x.keys.insert(i, y.keys[t - 1])
-
-        z.keys = y.keys[t:(2*t) - 1]
-        y.keys = y.keys[0: t - 1]
-
-        if not y.leaf:
-            z.children = y.children[t: 2 * t]
-            y.children = y.children[0: t - 1]
-    
+        self.t = t
+ 
     def insert(self, k):
-        t = self.order
         root = self.root
-
-        if len(root.keys) == 2 * t - 1:
-            new_root = BTreeNode()
-            self.root = new_root
-            new_root.children.insert(0, root)
-            self.split_child(new_root, 0)
-            self.insert_non_full(new_root, k)
+        if len(root.keys) == (2 * self.t) - 1:
+            temp = BTreeNode()
+            self.root = temp
+            temp.child.insert(0, root)
+            self.split_child(temp, 0)
+            self.insert_non_full(temp, k)
         else:
             self.insert_non_full(root, k)
-
+ 
     def insert_non_full(self, x, k):
-        t = self.order
+        global inserted
         i = len(x.keys) - 1
-
         if x.leaf:
-            x.keys.append(None)
+            x.keys.append((None, None))
             while i >= 0 and k < x.keys[i]:
                 x.keys[i + 1] = x.keys[i]
-                i -= 1
+                i -= 1     
             x.keys[i + 1] = k
+            inserted += 1  
+        else:
+            while i >= 0 and k < x.keys[i]:
+                i -= 1
+                i += 1
+            if len(x.child[i].keys) == (2 * self.t) - 1:
+                self.split_child(x, i)
+            if k > x.keys[i]:
+                i += 1
+            self.insert_non_full(x.child[i], k)
+ 
+    def split_child(self, x, i):
+        t = self.t
+        y = x.child[i]
+        z = BTreeNode(y.leaf)
+        x.child.insert(i + 1, z)
+        x.keys.insert(i, y.keys[t - 1])
+        z.keys = y.keys[t: (2 * t) - 1]
+        y.keys = y.keys[0: t - 1]
+        if not y.leaf:
+            z.child = y.child[t: 2 * t]
+            y.child = y.child[0: t - 1]
+ 
+    def print_tree(self, x, l=0):
+        print("Level ", l, " ", len(x.keys), end=":")
+        for i in x.keys:
+            print(i, end=" ")
+        print()
+        l += 1
+        if len(x.child) > 0:
+            for i in x.child:
+                self.print_tree(i, l)
+ 
+    def search_key(self, k, x=None):
+        if x is not None:
+            i = 0
+            while i < len(x.keys) and k > x.keys[i]:
+                i += 1
+            if i < len(x.keys) and k == x.keys[i]:
+                return (x, i)
+            elif x.leaf:
+                return None
+            else:
+                return self.search_key(k, x.child[i]) 
+        else:
+            return self.search_key(k, self.root)
 
-def test_btree():
 
-    btree = BTree(1005)
+
+
+def main():
+    global inserted
+    B = BTree(3)
     games = data.read_from_csv_file()
-    for game in games:
+    for i,game in enumerate(games):
         #print(int(game['appid']))
-        btree.insert(int(game['appid']))
+        B.insert(int(game['appid']))
+        if int(game['appid']) > 33333:
+          break
 
-    print(f"              {len(btree.root.keys)}")
-    print(f"{len(btree.root.children[0].keys)}                 {len(btree.root.children[1].keys)}")
-    print(f"{len(btree.root.children[0].keys)}                 {len(btree.root.children[1].keys)}")
+    B.print_tree(B.root)
+    print(inserted)
 
 
-test_btree()
+if __name__ == '__main__':
+    main()
